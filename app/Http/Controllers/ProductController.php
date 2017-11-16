93ebb4abed945dbb1d8f1a2e1d5c02872e37984c
Sean <smmcneal@gmail.com>
2017-11-16 14:12:25 -0500

    Finished thru video #10

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -33,4 +33,14 @@ class ProductController extends Controller
         return view('coffee.shopping-cart', ['products' => $cart->items, 'totalPrice' => $cart->totalPrice]);
 
     }
+
+    public function getCheckout() {
+        if (!Session::has('cart')) {
+            return view('coffee.shopping-cart');
+        }
+        $oldCart = Session::get('cart');
+        $cart = new Cart($oldCart);
+        $total = $cart->totalPrice;
+        return view('coffee.checkout', ['total'=>$total]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -64,3 +64,13 @@ Route::get('/', [
         'uses' => 'ProductController@getCart',
         'as' => 'product.shoppingCart'
     ]);
+
+    Route::get('/checkout', [
+        'uses' => 'ProductController@getCheckout',
+        'as' => 'checkout'
+    ]);
+
+    Route::post('/checkout', [
+        'uses' => 'ProductController@postCheckout',
+        'as' => 'checkout',
+    ]);
